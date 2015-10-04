58c3f9ec209971f8adf8e48a05554b6a0042428c
Barnabas Bulpett <bbulpett@charter.net>
2015-10-04 14:55:57 -0400

    create reviews form and ReviewController

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,6 +5,27 @@
 		this.products = gems;
 	});
 
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab ===checkTab;
+		};
+	});
+
+	app.controller("ReviewController", function(){
+		this.review = {};
+
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
 	var gems = [
 		{
 			name: 'Dodecahedron',
@@ -20,6 +41,18 @@
 				{
 					full: 'images/dodecahedron-02-full.jpg',
 					thumb: 'images/dodecahedron-02-thumb.jpg',
+				}
+			],
+			reviews: [
+				{
+					stars: 5,
+					body: "This is awesome!",
+					author: "joe@thomas.com"
+				},
+				{
+					stars: 1,
+					body: "This product is terrible",
+					author: "tim@hater.com"
 				}
 			]
 		},
